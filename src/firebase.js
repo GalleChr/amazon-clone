daa63758072125fa4b11bcaed34f2eaf003ad3d6
Christian Gallelli Pflaum <christian_gallelli@hotmail.com>
2021-01-08 10:35:32 -0300

    Header format finished

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD2PBbNrZNXYRYHs6ez5CkBuvFfVdgkKLc",
+    authDomain: "clone-ea5b2.firebaseapp.com",
+    projectId: "clone-ea5b2",
+    storageBucket: "clone-ea5b2.appspot.com",
+    messagingSenderId: "1054422841686",
+    appId: "1:1054422841686:web:c6564825d134e5baeafef7"
+  };
