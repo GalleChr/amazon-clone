8a342d384b3aa9c76da2246858410b1a25cfba1c
Christian Gallelli Pflaum <christian_gallelli@hotmail.com>
2021-01-09 11:25:47 -0300

    firebase auth done

---
+++ src/Login.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "./firebase";
+import "./Login.css";
+
+function Login() {
+  const history = useHistory();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // it successfully created a new user with email/pass
+        console.log(auth);
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img
+          className="login_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1920px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+
+      <div className="login_container">
+        <h1>Sign in</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <h5>Password</h5>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button className="login_signInButton" onClick={signIn} type="submit">
+            Sign In
+          </button>
+        </form>
+
+        <p>
+          By signing-in you agree to the Amazon Fake Clone Conditions of Use &
+          Sale. Check our Privacy Notice.
+        </p>
+
+        <button className="login_registerButton" onClick={register}>
+          Create your Amazon Account
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,17 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyD2PBbNrZNXYRYHs6ez5CkBuvFfVdgkKLc",
-    authDomain: "clone-ea5b2.firebaseapp.com",
-    projectId: "clone-ea5b2",
-    storageBucket: "clone-ea5b2.appspot.com",
-    messagingSenderId: "1054422841686",
-    appId: "1:1054422841686:web:c6564825d134e5baeafef7"
-  };
+  apiKey: "AIzaSyD2PBbNrZNXYRYHs6ez5CkBuvFfVdgkKLc",
+  authDomain: "clone-ea5b2.firebaseapp.com",
+  projectId: "clone-ea5b2",
+  storageBucket: "clone-ea5b2.appspot.com",
+  messagingSenderId: "1054422841686",
+  appId: "1:1054422841686:web:c6564825d134e5baeafef7",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
